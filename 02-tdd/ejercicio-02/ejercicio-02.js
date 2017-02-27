1b6fec2f78d3f528e1ae3ec999570136ef79912e
Ana Durand <ana.dc1004@gmail.com>
2017-02-27 13:15:16 -0500

    segundo ejercicio

--- 02-tdd/ejercicio-02/ejercicio-02.js
+++ 02-tdd/ejercicio-02/ejercicio-02.js
@@ -1,5 +1,24 @@
 function busquedaLinear (array,itemToSearch) {
   //Escribe tu codigo aqui
+
+var c = -1;
+  if(array == undefined || itemToSearch == undefined){
+    return "Error";
+  }else{
+    if(!isNaN(parseInt(itemToSearch))){
+      for(i=0; i<array.length; i++){
+        if(array[i]==itemToSearch){
+          c = i;
+          break;
+        }
+      }
+      if(c!=-1){ return c;}else{return false;}
+    }else{ return "Error de dato";}
+
+  }
+
+
+
 }
 
 //NO TOCAR ESTE CODIGO O EXPLOTARA LA PC EN 10 SEGUNDOS
